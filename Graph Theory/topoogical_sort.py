1645c69f4f19b4e82db7ede6f616346ac715ccbe
joshiprashanthd <58854114+joshiprashanthd@users.noreply.github.com>
2020-02-11 12:05:19 +0530

    A minor correction is made. append operation in dfs method is gonna execute after the neighbors are added into the visited nodes.

--- Graph Theory/topoogical_sort.py
+++ Graph Theory/topoogical_sort.py
@@ -7,7 +7,6 @@ class TopologicalSort:
         self.top_rev_order: List[int] = []
         self.visited: Dict[int, bool] = dict([(key, False) for key in self.graph.nodes()])
         
-        
     def run(self) -> list:
         
         for node in self.graph.nodes():
@@ -16,25 +15,28 @@ class TopologicalSort:
                 self.__dfs(node, visited_nodes)
                 for node in visited_nodes:
                     self.top_rev_order.append(node)
-        # self.top_rev_order.reverse()
+        self.top_rev_order.reverse()
         return self.top_rev_order
     
     def __dfs(self, at: int, visited_nodes: list):
         self.visited[at] = True
-        visited_nodes.append(at)
         
         edges = self.graph[at]
         
         for edge in edges:
             if not self.visited[edge.to]:
                 self.__dfs(edge.to, visited_nodes)
+                
+        visited_nodes.append(at)
             
 g = Graph[int]()
-g.add_edge_from(1, 3)
 g.add_edge_from(1, 2)
-g.add_edge_from(3, 5)
+g.add_edge_from(1, 3)
 g.add_edge_from(2, 5)
 g.add_edge_from(2, 6)
+g.add_edge_from(3, 5)
+g.add_edge_from(7, 1)
+g.add_edge_from(7, 8)
 
 ts = TopologicalSort(g)
 order = ts.run()
