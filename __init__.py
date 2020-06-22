58dd36e07ccb051e38df8c209f7ef901cac5a93a
prashantjoshi <58854114+joshiprashanthd@users.noreply.github.com>
2020-06-22 17:18:09 +0530

    Deleted from __init__.py file from algorithms directory

--- __init__.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-
-sys.path.append(os.path.abspath(os.path.join("graph_theory")))
-sys.path.append(os.path.abspath(os.path.join("graph_theory\\ds")))
-sys.path.append(os.path.abspath(os.path.join("graph_theory\\test")))
