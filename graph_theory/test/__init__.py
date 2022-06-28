cc1618eedab0ac8c190cabe222dc37d6a70a42b8
= <joshiprashanthd121@gmail.com>
2022-06-28 22:42:42 +0530

    chore: delete all tests for graph algorithms

--- graph_theory/test/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-
-sys.path.append(os.path.abspath(os.path.join("graph_theory")))
-sys.path.append(os.path.abspath(os.path.join("..","ds")))
-sys.path.append(os.path.abspath(os.path.join("..", "test")))
-
-# print(sys.path)
